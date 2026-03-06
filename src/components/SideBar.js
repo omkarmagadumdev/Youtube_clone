57734e221548b573de370cec431c1eea9369e7a2
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-03-06 13:07:16 +0000

    Refactor layout of Body, MainContainer, and SideBar components for improved structure

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,8 +2,15 @@ import React from "react"
 
 const SideBar = () => {
   return (
-    <div>
-
+    <div className="p-5 shadow-lg">
+          <h1>Subscription</h1>
+          <ul>
+            <li>Music</li>
+            <li>sports</li>
+            <li>Gaming</li>
+            <li>movies</li>
+          </ul>
+        
     </div>
 
   )
